70b0c5d83705debb6d70a29e0c3ea5ad3e56675a
e-dvni <danielslee078@gmail.com>
2025-12-26 13:05:26 -0500

    Fixed more Deploy Issues

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,18 +1,13 @@
-# api/config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # In production on Render, allow your deployed frontend.
-    # In development, allow Vite dev server.
     origins(
-      ENV.fetch("FRONTEND_ORIGIN", "https://portfolio-os-web.onrender.com"),
-      "http://localhost:5173"
+      ENV.fetch("FRONTEND_ORIGIN", "http://localhost:5173"),
+      "https://portfolio-os-web.onrender.com"
     )
 
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head],
-      expose: [],
-      credentials: false
+      expose: ["Authorization"]
   end
 end
